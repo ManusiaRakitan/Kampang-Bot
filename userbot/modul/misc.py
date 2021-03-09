1a540e0fa4f4b5e7a6c1e2db8e9b1b2bff483941
Musthofaalfian <78151948+Musthofaalfian@users.noreply.github.com>
2021-03-10 01:23:17 +0700

    Update and rename userbot/modules/misc.py to userbot/modul/misc.py

--- userbot/modul/misc.py
+++ userbot/modul/misc.py
@@ -65,7 +65,7 @@ async def sleepybot(time):
 @register(outgoing=True, pattern="^.shutdown$")
 async def killdabot(event):
     """ For .shutdown command, shut the bot down."""
-    await event.edit("`Shutdown *BOT KAMPANG 🐨*....`")
+    await event.edit("`Shutdown *BOT ANCIENT 🕵️*....`")
     await asyncio.sleep(7)
     await event.delete()
     if BOTLOG:
@@ -76,7 +76,7 @@ async def killdabot(event):
 
 @register(outgoing=True, pattern="^.restart$")
 async def killdabot(event):
-    await event.edit("`Restarting BOT KAMPANG🐨....`")
+    await event.edit("`Restarting BOT ANCIENT🕵️....`")
     await asyncio.sleep(10)
     await event.delete()
     if BOTLOG:
@@ -120,7 +120,7 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "╭••••••••𖠄••••••••╮🐨╭••••••••𖠄••••••••╮\n \n[┣━•━─[ B🐨T°KAMPANG ]─━•━┫](https://github.com/ManusiaRakitan/Kampang-Bot) \n \n╰••••••••𖠄••••••••╯🐨╰••••••••𖠄••••••••╯\n     \n"
+        "🌹Pemilik: [Ancient] (https://t.me/ancientbatavia) \n \n [B⛔️T°ANCIENT] (https://github.com/Musthofaalfian/Kampang-Bot) \n \n BOT-MIX [Leo](https://t.me/leoangkasaaa) & [Lord Koala](https://t.me/manusiarakitann) \n     \n"
     )
 
 
